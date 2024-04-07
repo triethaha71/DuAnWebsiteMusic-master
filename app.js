79037605b485608953ac9adb023b69ebb63f3171
triethaha71 <triethaha71@gmail.com>
2024-04-07 22:35:42 +0700

    a

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 let pop_song_left = document.getElementById('pop_song_left');
 let pop_song_right = document.getElementById('pop_song_right');
-let pop_song = document.getElementsByClassName('songItem')[0];
+let pop_song = document.getElementsByClassName('pop_song')[0];
 
 pop_song_right.addEventListener('click', () =>{
     pop_song.scrollLeft += 330;
