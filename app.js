209f7dcd96d510d4c72568f20f5609ffc540d339
Hoàng <hoangsach2003@gmail.com>
2024-04-07 22:36:27 +0700

    add

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 let pop_song_left = document.getElementById('pop_song_left');
 let pop_song_right = document.getElementById('pop_song_right');
-let pop_song = document.getElementsByClassName('songItem')[0];
+let pop_song = document.getElementsByClassName('pop_song')[0];
 
 pop_song_right.addEventListener('click', () =>{
     pop_song.scrollLeft += 330;
@@ -15,12 +15,12 @@ pop_song_left.addEventListener('click', () =>{
 
 let pop_art_left = document.getElementById('pop_art_left');
 let pop_art_right = document.getElementById('pop_art_right');
-let item = document.getElementsByClassName('pop_art')[0];
+let item = document.getElementsByClassName('item')[0];
 
 pop_art_left.addEventListener('click', () =>{
     item.scrollLeft += 330;
 })
 
 pop_art_right.addEventListener('click', () =>{
-    pop_art.scrollLeft -= 330;
+    item.scrollLeft -= 330;
 })
